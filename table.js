8f14e1f5888d0def056e1632c522e3ce64d8a453
Basile Henry <basile.henry@hotmail.com>
2013-02-19 15:39:20 +0000

    table with dates

---
+++ table.js
@@ -0,0 +1,58 @@
+
+window.onload=function() {
+	makeTable();
+	}
+
+function makeTable() {
+
+	row=new Array();
+	cell=new Array();
+	date=new Array();	
+
+	var dat = new Date();
+
+	row_num=1000; //edit this value to suit
+	cell_num=1; //edit this value to suit
+
+	tab=document.createElement('table');
+	tab.setAttribute('id','newtable');
+
+	tbo=document.createElement('tbody');
+
+	for(c=0;c<row_num;c++){
+	row[c]=document.createElement('tr');
+	date[c]= dat.getDate() + "/" + (dat.getMonth()+1) + "/" + dat.getFullYear();
+	dat.setDate(dat.getDate() + 1);
+	
+	
+	
+		for(k=0;k<cell_num;k++) {
+		cell[k]=document.createElement('td');
+
+		var bt = document.createElement('input');
+		bt.type = 'button';
+		bt.style.backgroundColor = 'transparent';
+		bt.value = date[c];
+		bt.onclick = changeColor;
+
+		cell[k].appendChild(bt);
+		row[c].appendChild(cell[k]);
+		}
+	
+	tbo.appendChild(row[c]);
+	}
+	tab.appendChild(tbo);
+	document.getElementById('mytable').appendChild(tab);
+	}
+
+function changeColor() {
+	
+	
+	this.style.backgroundColor = 'red';
+	
+
+	}
+
+
+
+
