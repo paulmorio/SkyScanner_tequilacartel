a725d0084747773eb650af47a1987c4d94ad536d
Andrej Ivanis <anivanis@gmail.com>
2013-02-22 12:35:45 +0000

    icons

--- map.js
+++ map.js
@@ -48,14 +48,14 @@ function changeOrigin(coors){
 		oriCirc.setLatLng(coors);
 	}else{
 		var greenIcon = L.icon({
-		    iconUrl: 'arrow.png',
+		    iconUrl: 'homeIconSmall.png',
 		    //shadowUrl: 'leaf-shadow.png',
 
-		    iconSize:     [38, 95], // size of the icon
-		    shadowSize:   [50, 64], // size of the shadow
-		    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-		    shadowAnchor: [4, 62],  // the same for the shadow
-		    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+		    iconSize:     [40, 50], // size of the icon
+		    //shadowSize:   [50, 64], // size of the shadow
+		    iconAnchor:   [20, 49], // point of the icon which will correspond to marker's location
+		    //shadowAnchor: [4, 62],  // the same for the shadow
+		    //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 		});
 		oriCirc = L.marker(coors, {icon: greenIcon});
 		//console.log(oriCirc);
@@ -69,6 +69,16 @@ alert(document.getElementById("budget").value);
 	var maxH = document.getElementById("budget").value/10;
 	for(i =0; i<maxH;i++){
 		cities = citiesByPrices[i];
+		var greenIcon = L.icon({
+		    iconUrl: 'arrow.png',
+		    //shadowUrl: 'leaf-shadow.png',
+
+		    iconSize:     [40, 51], // size of the icon
+		    //shadowSize:   [50, 64], // size of the shadow
+		    iconAnchor:   [20, 50], // point of the icon which will correspond to marker's location
+		    //shadowAnchor: [4, 62],  // the same for the shadow
+		    //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+		});
 		oriCirc = L.marker(cityCoordinates[cities[0].destination]);
 	}
 }
